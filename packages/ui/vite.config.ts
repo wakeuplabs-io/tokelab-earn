70c2589f98803e9baa044857c6136253c33846d0
Nahuel Jaume <nahueljaume@gmail.com>
2026-01-02 10:45:15 -0300

    postcss config update tailwind v4

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
