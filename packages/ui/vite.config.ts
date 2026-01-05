5f300960a06725a2812e6fed87972fb72aeeab63
Nahuel Jaume <nahueljaume@gmail.com>
2026-01-05 17:04:18 -0300

    plugin react vite

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,5 +1,5 @@
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
